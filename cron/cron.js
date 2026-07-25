655af051a35f03dbe470a3b229d5a0bc3b339188
aaron2160 <aaron2160@noreply.gitcode.com>
2026-07-24 22:58:48 -0300

    frontend update

--- cron/cron.js
+++ cron/cron.js
@@ -1,10 +1,10 @@
 import cron from 'node-cron';
 import { sendAppointmentReminders } from './tasks.js';
 
-cron.schedule('* * * * *', async () => {
-    await sendAppointmentReminders();
-});
-
-// cron.schedule('1,31 * * * *', async () => {
+// cron.schedule('* * * * *', async () => {
 //     await sendAppointmentReminders();
 // });
+
+cron.schedule('1,31 * * * *', async () => {
+    await sendAppointmentReminders();
+});
